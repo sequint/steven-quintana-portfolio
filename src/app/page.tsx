f2550b0b9e3562ca77916c83daaff3c7f29ff86a
stevenquintana <steve.e.quint@gmail.com>
2024-01-18 14:29:48 -0800

    Remove old styles import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import SiteLogo from './components/SiteLogo'
 import TitleDescription from './components/TitleDescription'
-import styles from './app.module.css'
 
 export default function Home() {
   return (

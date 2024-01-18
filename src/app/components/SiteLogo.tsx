52c051398271dbf1c7e5a55f126db67afbd2e0d4
stevenquintana <steve.e.quint@gmail.com>
2024-01-18 13:51:25 -0800

    Give logo a slight tilt

--- src/app/components/SiteLogo.tsx
+++ src/app/components/SiteLogo.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 export default function SiteLogo() {
   return (
     <Image
+      style={{ transform: 'rotate(349deg)' }}
       src={ logo }
       width={ 300 }
       height={ 300 }

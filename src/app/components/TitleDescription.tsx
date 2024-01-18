c59f3b7105040c910bae8fe58d6ac2345f75fa74
stevenquintana <steve.e.quint@gmail.com>
2024-01-18 15:32:43 -0800

    Update description with data file and update styles

--- src/app/components/TitleDescription.tsx
+++ src/app/components/TitleDescription.tsx
@@ -1,3 +1,4 @@
+import { description } from '@/data/description'
 import { fredoka } from '@/utils/fonts'
 import styles from './TitleDescription.module.css'
 
@@ -5,7 +6,17 @@ export default function TitleDescription() {
   return (
     <>
       <h1 className={ `${fredoka.className} ${styles.title}`}>Steven Quintana</h1>
-      <article></article>
+      <article
+        style={{ 
+          fontFamily: 'Arial',
+          fontSize: 'large',
+          width: '30dvw'
+        }}
+      >
+        <p>{ description.p1 }</p>
+        <p>{ description.p2 }</p>
+        <p>{ description.p3 }</p>
+      </article>
     </>
   )
 }
---
+++ src/data/description.ts
@@ -0,0 +1,7 @@
+export const description = {
+  p1: `I am a software engineer that loves to create unique software that promotes community and a healthier planet!`,
+  p2: `I have two years of experience working as a developer in
+  FinTech creating a CRM platform and in the public sector.`,
+  p3: `I am also currently pursuing an MS in Computer Science
+  at the University of Massachusetts, Dartmouth.`
+}

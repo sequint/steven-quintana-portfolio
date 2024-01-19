a4f230f67d6eda045b761ec98f84beaa7d4bba0a
stevenquintana <steve.e.quint@gmail.com>
2024-01-19 12:05:00 -0800

    Create fade in transition for description

--- src/app/components/TitleDescription.tsx
+++ src/app/components/TitleDescription.tsx
@@ -4,7 +4,7 @@ import styles from './TitleDescription.module.css'
 
 export default function TitleDescription() {
   return (
-    <>
+    <div className={ styles.fadeIn }>
       <h1 className={ `${fredoka.className} ${styles.title}`}>Steven Quintana</h1>
       <article
         style={{ 
@@ -17,6 +17,6 @@ export default function TitleDescription() {
         <p>{ description.p2 }</p>
         <p>{ description.p3 }</p>
       </article>
-    </>
+    </div>
   )
 }

8c0eb5197bc982bc9c0a239d921ba60117725065
stevenquintana <steve.e.quint@gmail.com>
2024-01-18 16:25:33 -0800

    Create IconLinks component

---
+++ src/app/components/IconLinks.tsx
@@ -0,0 +1,26 @@
+import { FiGithub } from 'react-icons/fi'
+import { FiLinkedin } from 'react-icons/fi'
+import styles from './IconLinks.module.css'
+import Link from 'next/link'
+
+export default function IconLinks() {
+  return (
+    <div style={{ marginRight: 100 }}>
+      <Link href='https://github.com/sequint' target='_blank'>
+        <FiGithub
+          size={35}
+          color='grey'
+          className={ styles.icon }
+        />
+      </Link>
+      <Link href='https://www.linkedin.com/in/steven-quintana-278b0091/' target='_blank'>
+        <FiLinkedin
+          size={35}
+          color='grey'
+          className={ styles.icon }
+          style={{ marginLeft: 20 }}
+        />
+      </Link>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import SiteLogo from './components/SiteLogo'
 import TitleDescription from './components/TitleDescription'
+import IconLinks from './components/IconLinks'
+import { FiGithub } from 'react-icons/fi'
 
 export default function Home() {
   return (
     <main>
-      <div style={{ width: '42dvw', textAlign: 'right' }}>
-        <SiteLogo />
+      <div style={{ display: 'inline-flex' }}>
+        <div style={{ width: '42dvw', textAlign: 'right' }}>
+          <SiteLogo />
+        </div>
+        <div style={{ width: '58dvw', marginLeft: 50 }}>
+          <TitleDescription />
+        </div>
       </div>
-      <div style={{ width: '58dvw', marginLeft: 50 }}>
-        <TitleDescription />
+      <div style={{ display: 'inline-flex'}}>
+        <div style={{ width: '42dvw', textAlign: 'right' }}>
+          <IconLinks />
+        </div>
       </div>
     </main>
   )

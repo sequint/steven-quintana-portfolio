12a7a7d8691ed6351fc6dcd627f6bcaac6f35287
stevenquintana <steve.e.quint@gmail.com>
2024-01-21 12:54:58 -0800

    Change description width to 50 to avoid overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import SiteLogo from './components/SiteLogo'
 import TitleDescription from './components/TitleDescription'
 import IconLinks from './components/IconLinks'
+import BtmImageLinks from './components/BtmImageLinks'
 
 export default function Home() {
   return (
@@ -9,14 +10,17 @@ export default function Home() {
         <div style={{ width: '42dvw', textAlign: 'right' }}>
           <SiteLogo />
         </div>
-        <div style={{ width: '58dvw', marginLeft: 50 }}>
+        <div style={{ width: '50dvw', marginLeft: 50 }}>
           <TitleDescription />
         </div>
       </div>
-      <div style={{ display: 'inline-flex'}}>
+      <div>
         <div style={{ width: '42dvw', textAlign: 'right' }}>
           <IconLinks />
         </div>
+      </div>
+      <div>
+        <BtmImageLinks />
       </div>
     </main>
   )

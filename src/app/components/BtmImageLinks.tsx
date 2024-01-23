d2074e7b3fdd591b22a1fa6b472bd09487c9899e
stevenquintana <steve.e.quint@gmail.com>
2024-01-23 11:18:07 -0800

    Complete image link hover effects - Change state to contain an obejct which controls img opacit and title display vals - Update title with same hover functionality

--- src/app/components/BtmImageLinks.tsx
+++ src/app/components/BtmImageLinks.tsx
@@ -1,48 +1,84 @@
 'use client'
 
 import Image from 'next/image'
+import { fredoka } from '@/utils/fonts'
 import skillsIcon from '../images/skillsIcon.png'
 import projectsIcon from '../images/projectsIcon.png'
 import experienceIcon from '../images/experienceIcon.png'
 import styles from './BtmImageLinks.module.css'
 import { useState } from 'react'
 
+type ImageLink = {
+  imgOpacity: number,
+  titleDisplay: string
+}
+
 export default function BtmImageLinks() {
-  const [ skillsOpacity, setSkillsOpacity ] = useState(1)
-  const [ projectsOpacity, setProjectsOpacity ] = useState(1)
-  const [ experienceOpacity, setExperienceOpacity ] = useState(1)
+  const [ skillsImg, setSkillsImg ] = useState<ImageLink>({
+    imgOpacity: 1, titleDisplay: 'none'
+  })
+  const [ projectsImg, setProjectsImg ] = useState<ImageLink>({
+    imgOpacity: 1, titleDisplay: 'none'
+  })
+  const [ experienceImg, setExperienceImg ] = useState<ImageLink>({
+    imgOpacity: 1, titleDisplay: 'none'
+  })
 
   return (
     <div className={ styles.imageContainer }>
+      <span
+        style={{ display: skillsImg.titleDisplay }}
+        className={ `${fredoka.className} ${styles.title} ${styles.skillsTitle}` }
+        onMouseEnter={() => setSkillsImg({ imgOpacity: .4, titleDisplay: 'inline' })}
+        onMouseLeave={() => setSkillsImg({ imgOpacity: 1, titleDisplay: 'none' })}
+      >
+        Skills
+      </span>
       <Image
-        style={{ opacity: skillsOpacity }}
+        style={{ opacity: skillsImg.imgOpacity }}
         className={ styles.image }
         src={ skillsIcon }
         width={ 200 }
         height={ 200 }
         alt='HTML Element Brackets'
-        onMouseEnter={() => setSkillsOpacity(.4)}
-        onMouseLeave={() => setSkillsOpacity(1)}
+        onMouseEnter={() => setSkillsImg({ imgOpacity: .4, titleDisplay: 'inline' })}
+        onMouseLeave={() => setSkillsImg({ imgOpacity: 1, titleDisplay: 'none' })}
       />
+      <span
+        style={{ display: projectsImg.titleDisplay }}
+        className={ `${fredoka.className} ${styles.title} ${styles.projectsTitle}` }
+        onMouseEnter={() => setProjectsImg({ imgOpacity: .4, titleDisplay: 'inline' })}
+        onMouseLeave={() => setProjectsImg({ imgOpacity: 1, titleDisplay: 'none' })}
+        >
+          Project Highlights
+      </span>
       <Image
-        style={{ marginLeft: 50, opacity: projectsOpacity }}
+        style={{ marginLeft: 50, opacity: projectsImg.imgOpacity }}
         className={ styles.image }
         src={ projectsIcon }
         width={ 325 }
         height={ 325 }
         alt='Computer'
-        onMouseEnter={() => setProjectsOpacity(.4)}
-        onMouseLeave={() => setProjectsOpacity(1)}
+        onMouseEnter={() => setProjectsImg({ imgOpacity: .4, titleDisplay: 'inline' })}
+        onMouseLeave={() => setProjectsImg({ imgOpacity: 1, titleDisplay: 'none' })}
       />
+      <span
+        style={{ display: experienceImg.titleDisplay }}
+        className={ `${fredoka.className} ${styles.title} ${styles.experienceTitle}` }
+        onMouseEnter={() => setExperienceImg({ imgOpacity: .4, titleDisplay: 'inline' })}
+        onMouseLeave={() => setExperienceImg({ imgOpacity: 1, titleDisplay: 'none' })}
+        >
+        Experience
+      </span>
       <Image
-        style={{ opacity: experienceOpacity }}
+        style={{ opacity: experienceImg.imgOpacity }}
         className={ styles.image }
         src={ experienceIcon }
         width={ 410 }
         height={ 410 }
         alt='Office Building'
-        onMouseEnter={() => setExperienceOpacity(.4)}
-        onMouseLeave={() => setExperienceOpacity(1)}
+        onMouseEnter={() => setExperienceImg({ imgOpacity: .4, titleDisplay: 'inline' })}
+        onMouseLeave={() => setExperienceImg({ imgOpacity: 1, titleDisplay: 'none' })}
       />
     </div>
   )

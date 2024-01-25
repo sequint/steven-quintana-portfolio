896437e5597887788b4ac9a1636b0fcb325ecf6a
stevenquintana <steve.e.quint@gmail.com>
2024-01-25 13:49:30 -0800

    Update description style

--- src/app/components/Description.tsx
+++ src/app/components/Description.tsx
@@ -1,15 +1,9 @@
-import { description } from '@/data/description'
+import styles from './Description.module.css'
 
 export default function Description() {
   return (
-    <article
-      style={{ 
-        fontFamily: 'Arial',
-        fontSize: 'large',
-        width: '30dvw'
-      }}
-    >
-      <p>{ description.p1 }</p>
+    <article className={ styles.descriptionContent }>
+      <p>I build simple, effecient, and beautiful products for the web and native applications.</p>
     </article>
   )
 }

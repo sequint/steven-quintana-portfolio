0771f71143347321e8c527c17d45b093ba213d35
stevenquintana <steve.e.quint@gmail.com>
2024-02-21 15:24:28 -0800

    Add resume to resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,13 +1,11 @@
-
 export default function Resume() {
   return (
-    <div>
+    <div style={{ width: '100dvw', height: '100dvh'}}>
       <embed
-        src="/Steven_Quintana_Resume.pdf"
+        src='/Steven_Quintana_Resume.pdf'
         width='100%'
         height='100%'
-      >
-      </embed>
+      />
     </div>
   )
 }

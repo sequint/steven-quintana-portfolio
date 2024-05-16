750e97415214e0f11b8659af86c176824682c134
stevenquintana <steve.e.quint@gmail.com>
2024-05-16 09:16:36 -0700

    Remove bg hover effect and replace with color fade on hover

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,12 +1,8 @@
-import { pageTitles } from '@/data/pageTitles'
 import { fredoka } from '@/utils/fonts'
 import Link from 'next/link'
 import styles from './NavBar.module.css'
 
 export default function NavBar() {
-  // Add rounded nav bar
-  // Below description add list of education, projects, and experience
-  // Each list item is an arrow dropdown that exposes it on hover
   return (
     <nav className={ `${ fredoka.className } ${ styles.navContainer }` }>
       <div className={ styles.linksContainer }>

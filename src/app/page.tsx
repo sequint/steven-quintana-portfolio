f26c4d1f988a89bdcd79f695ca25ac91c5c35858
stevenquintana <steve.e.quint@gmail.com>
2024-05-16 09:34:59 -0700

    Update margins to remove horizontal scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,17 @@ export default function Home() {
     <>
       <NavBar></NavBar>
       <main>
-        <div style={{ display: 'inline-flex', marginTop: 50 }}>
+        <div style={{ display: 'inline-flex', marginTop: 150 }}>
           <div style={{ width: '50dvw', textAlign: 'right' }}>
             <SiteLogo />
           </div>
-          <div style={{ width: '50dvw', marginLeft: 50 }}>
-            <h1 className={ `${ fredoka.className } title`}>Steven Quintana</h1>
-            <div className='fadeIn'>
-              <Description />
-              <ProjectHighlights />
+          <div style={{ width: '50dvw' }}>
+            <div style={{ marginLeft: 50 }}>
+              <h1 className={ `${ fredoka.className } title`}>Steven Quintana</h1>
+              <div className='fadeIn'>
+                <Description />
+                <ProjectHighlights />
+              </div>
             </div>
           </div>
         </div>

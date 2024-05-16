c0671d4e3382aab3eef91a048928dc1f906542ea
stevenquintana <steve.e.quint@gmail.com>
2024-05-16 12:50:58 -0700

    Update text and image alignment

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { fredoka } from '@/utils/fonts'
 import headshot from '../images/headshot.png'
 import styles from './AboutMe.module.css'
 
@@ -6,7 +7,10 @@ export default function AboutMe() {
   return (
     <div className={ styles.aboutContainer }>
       <div className={ styles.aboutDescContainer }>
-        <p>hello</p>
+        <div className={ `${ fredoka.className } ${styles.textContainer }`}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
       </div>
       <div className={ styles.aboutImgContainer }>
         <Image
